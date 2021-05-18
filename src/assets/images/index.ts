a6bf702732cb227270b3e74e4192c053d860868e
edsonpaulo <edsonpaulo24@gmail.com>
2021-05-18 04:54:37 +0100

    feat: images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as SoccerImg } from './soccer.png';
+export { default as LandingImg } from './mbappe.png';
+export { default as AutomobilismImg } from './automobilism.png';
+export { default as athletiSmImg } from './athletism.png';
+
+export { default as BallSVG } from './ball';
