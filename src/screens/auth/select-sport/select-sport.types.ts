9d75ad8ebcdf01c23e36342c06827aa3beba196a
edsonpaulo <edsonpaulo24@gmail.com>
2021-06-25 11:43:23 +0100

    refactor: delete files

--- src/screens/auth/select-sport/select-sport.types.ts
+++
@@ -1,8 +0,0 @@
-import { ImageSourcePropType } from 'react-native';
-
-export interface SportProps {
-  name: string;
-  img: ImageSourcePropType;
-  selectedSport: string;
-  onSelect: (name: string) => void;
-}
